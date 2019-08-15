a784de3191ae8a4701d2629699fc6f723ae8f15e
Edivânia Pontes <edivaniapts@gmail.com>
2019-08-15 12:49:48 -0300

    part 1

---
+++ AulaServlet/WebContent/login.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Login</title>
+</head>
+<body>
+	<form action="logar" method="post">
+		<h1>Bem vindo!</h1>
+		<fieldset>
+			<p>
+				<input type="text" name="login" placeholder="Usuário..." required="required"/>
+			</p>
+			<p>
+				<input type="password" name="senha" placeholder="Senha..." required="required"/>
+			</p>
+			<p>
+				<input type="submit" value="Logar"/>
+			</p>
+		</fieldset>
+	</form>
+</body>
+</html>
---
+++ AulaServlet/src/br/ufrn/imd/aulaservlet/seguranca/SegurancaFilter.java
@@ -0,0 +1,54 @@
+package br.ufrn.imd.aulaservlet.seguranca;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class SegurancaFilter
+ */
+@WebFilter("/paginas/*")
+public class SegurancaFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public SegurancaFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		
+		Usuario usuario = (Usuario) req.getSession().getAttribute("usuario");
+		
+		if(usuario == null)
+			res.sendRedirect("/AulaServlet/login.jsp");
+		else
+			chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
